1327286f943f4ab30b7b8c5996dd1cd31b2f7e85
Kevin Allen <kma1660@gmail.com>
2018-05-08 18:33:16 -0400

    SCAN THE CODE: refactor

--- perception/navigator_vision/nodes/scan_the_code.py
+++ perception/navigator_vision/nodes/scan_the_code.py
@@ -3,21 +3,29 @@ import rospy
 import cv2
 import numpy as np
 from mil_ros_tools import Image_Subscriber, Image_Publisher, rosmsg_to_numpy
-from mil_vision_tools import auto_canny, RectFinder, ImageMux, contour_mask, putText_ul, roi_enclosing_points
+from mil_vision_tools import auto_canny, RectFinder, ImageMux, contour_mask, putText_ul, roi_enclosing_points, rect_from_roi, Threshold
 from collections import deque
 from navigator_vision import ScanTheCodeClassifier
 from mil_msgs.srv import ObjectDBQuery, ObjectDBQueryRequest
 import tf
 from image_geometry import PinholeCameraModel
+from threading import Lock
+from copy import copy
+from std_msgs.msg import String
 
 
 class ScanTheCodePerception(object):
     LED_HEIGHT = 0.38608
     LED_WIDTH = 0.19304
+    ROI_MIN_Y = -0.7
+    ROI_MAX_Y = 0
+
 
     def __init__(self):
+        self.lock = Lock()
         self.enabled = False
         self.tf_listener = tf.TransformListener()
+        self.debug = False
         self.roi = None
         self.img = None
         self.get_params()
@@ -27,14 +35,22 @@ class ScanTheCodePerception(object):
         info = self.sub.wait_for_camera_info()
         self.camera_model = PinholeCameraModel()
         self.camera_model.fromCameraInfo(info)
-        if self.debug:
-            self.debug_pub = Image_Publisher('~debug_image')
-            res = 2
-            self.image_mux = ImageMux(size=(info.height * res, info.width * res), shape=(2, 2),
-                                      labels=['Original', 'Blur', 'Contours', 'Classification'])
+        self.update_roi()  # Update region of interest at start if possible
+        self.debug_pub = Image_Publisher('~debug_image')
+        res = 2
+        self.image_mux = ImageMux(size=(info.height * res, info.width * res), shape=(2, 2),
+                                  labels=['Original', 'Threshold', 'Edges', 'Classification'])
+        self.solution_pub = rospy.Publisher('~solution', String, queue_size=1)
         self.classification_list = deque()  # "DECK - AH - WAY - WAY"
         self.enabled = True
-        rospy.Timer(rospy.Duration(0.25), self.update_roi)
+        rospy.Timer(rospy.Duration(0.25), self.update_roi)  # Regularly update ROI
+
+    def filter_stc_points(self, points):
+        '''
+        Filter out some of the 3D points in camera frame via heuristics such as a range of heights (y).
+        For now does nothing because camera tf is off, making this impossible.
+        '''
+        return points
 
     def get_params(self):
         '''
@@ -43,18 +59,18 @@ class ScanTheCodePerception(object):
         '''
         self.debug = rospy.get_param('~debug', True)
         self.image_topic = rospy.get_param('~image_topic', '/camera/seecam/image_rect_color')
-        self.min_contour_area = rospy.get_param('~min_contour_area', 50)
+        self.min_contour_area = rospy.get_param('~min_contour_area', 250)
         self.filter_d = rospy.get_param('filter_d', 5)
         self.filter_sigma = rospy.get_param('filter_sigma', 50)
+        self.threshold = Threshold.from_dict(rospy.get_param('~threshold', {'LAB':[[150, 130, 100], [255, 140, 130]]}), in_space='BGR')
+        self.roi_border = rospy.get_param('~roi_border', (20, 0))
         self.classifier = ScanTheCodeClassifier()
         self.classifier.train_from_csv()
 
-    def update_roi(self, timer_obj):
+    def update_roi(self, timer_obj=None):
         '''
         Update the region of interest where the LED panel os
         '''
-        if not self.enabled:  # Ignore if not enabled
-            return
         # Get points from database
         try:
             res = self.db_service(ObjectDBQueryRequest(name='stc'))
@@ -77,31 +93,43 @@ class ScanTheCodePerception(object):
         R = tf.transformations.quaternion_matrix(rot)[:3, :3]
         points = rosmsg_to_numpy(obj.points)
         points_transformed = P + (R.dot(points.T)).T
-        self.roi = roi_enclosing_points(self.camera_model, points_transformed)
+        points_transformed = self.filter_stc_points(points_transformed)
+
+        # Set ROI
+        self.lock.acquire()
+        self.roi = roi_enclosing_points(self.camera_model, points_transformed, border=self.roi_border)
+        self.lock.release()
 
     def get_stc_mask(self, img):
-        # gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        blur = cv2.bilateralFilter(img[self.roi], 9, 75, 75)
-        edges = auto_canny(blur, sigma=0.5)
-
+        '''
+        Return the contour corosponding to the LED panel of scan the code in the image, if any contours
+        meet the criteria. The algorithim does the following:
+        1) Convert to LAB and blur (gaussian)
+        2) Loosely threshold in LAB colorspace to making edge detection easier
+        3) Detect edges with Canny
+        4) Find contours in edge image
+        5) Filter out contours with below a fixed area threshold
+        6) Sort remaining contours by their match to the model of the rectangle
+        7) Traverse sorted list, finding first one which can be approximated as a rectangle
+        '''
+        blur = cv2.GaussianBlur(img, (11, 11), 20)
+        threshold = self.threshold(blur)
+        edges = auto_canny(threshold)
         if self.debug:
-            self.image_mux[1] = blur
+            self.image_mux[1] = threshold
             self.image_mux[2] = edges
-        return None
         c, contours, hierarchy = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
         filtered_contours = filter(lambda c: cv2.contourArea(c) > self.min_contour_area, contours)
         if len(filtered_contours) == 0:
-            print 'none after area fiter'
+            rospy.logdebug('No contours meeting area minimum')
             return None
 
         matches = map(self.rect_finder.verify_contour, filtered_contours)
         matches_sorted_index = np.argsort(matches)
-        print 'best', matches[matches_sorted_index[0]]
-        if matches[matches_sorted_index[0]] > 1.0:
-            print 'no good rectangle'
-            return None
-        sorted_matches = np.array(matches)[matches_sorted_index]
+        sorted_matches = np.array(filtered_contours)[matches_sorted_index]
+        rospy.loginfo('best {}'.format(matches[matches_sorted_index[0]]))
+        rospy.loginfo('worse {}'.format(matches[matches_sorted_index[-1]]))
         match = None
         for i in sorted_matches:
             pts = self.rect_finder.get_corners(i, epsilon_range=(0.01, 0.05))
@@ -115,33 +143,78 @@ class ScanTheCodePerception(object):
                 continue
             match = i
             break
+        if match is None:
+            rospy.logdebug('No contours which can be approximated as rectangle')
+            return
         return match
 
+    def detect_pattern(self):
+        '''
+        Looks at most recent 5 detected colors to find a 3 color pattern solution.
+        If the list is in the form ['o', 'x', 'x', 'x', 'o'], with 3 colors ('x') surrounded
+        on either side by an off ('o'), return those 3 colors in the middle as a string like 'rgb'.
+        Otherwise, return None
+        '''
+        if len(self.classification_list) != 5:
+            return None
+        if self.classification_list[0] == 'o' and self.classification_list[4] == 'o'\
+           and self.classification_list[1] != 'o' and self.classification_list[2] != 'o'\
+           and self.classification_list[3] != 'o':
+            solution = self.classification_list[1] + self.classification_list[2] + self.classification_list[3]
+            self.solution_pub.publish(solution)
+            rospy.loginfo('SOLUTIONS ' + solution)
+            return solution
+        return None
+
+    def classify_panel(self, img, contour):
+        '''
+        Classify the color (or off) of the LED panel and update the buffer
+        of recent classifications.
+        '''
+        # Get classification index and string from classifier
+        mask = contour_mask(contour, img_shape=img.shape)
+        prediction = self.classifier.classify(img, mask)[0]  # class index, ex: 0
+        label = self.classifier.CLASSES[prediction]  # class string, ex: 'stc_red'
+        symbol = label[4]  # one character representation of label ex: 'r'
+
+        # update buffer of recent colors if it has changed
+        if len(self.classification_list) == 0 or self.classification_list[-1] != symbol:
+            if len(self.classification_list) >= 5:  # Only keep most recent 5 colors (needed to see pattern)
+                self.classification_list.popleft()
+            self.classification_list.append(symbol)
+            self.detect_pattern()
+        rospy.loginfo('FOUND: ' + ''.join(self.classification_list))
+        if self.debug:
+            debug = cv2.bitwise_or(img, img, mask=mask)
+            putText_ul(debug, label, (0, 0), fontScale=1, thickness=2)
+            self.image_mux[3] = debug
+
     def img_cb(self, img):
-        if not self.enabled:
+        if not self.enabled:  # Skip if not initialized/enabled
             return
-        if self.roi is None:
+
+        # Copy ROI locally
+        self.lock.acquire()
+        roi = copy(self.roi)
+        self.lock.release()
+        if roi is None:  # Skip if ROI not found yet
             rospy.logwarn_throttle(1.0, 'no roi')
             return
-        self.image_mux[0] = img
-        match = self.get_stc_mask(img)
-        if match is not None:
-            debug = contour_mask(match, img_shape=img.shape)
-            prediction = self.classifier.classify(img, debug)
-            label = self.classifier.CLASSES[prediction]
-            symbol = label[0]
-            if len(self.classification_list) == 0 or self.classification_list[-1] != symbol:
-                if len(self.classification_list) >= 5:
-                    self.classification_list.popleft()
-                self.classification_list.append(symbol)
-            text = label + ' | ' + ''.join(self.classification_list)
-            scale = 3
-            thickness = 2
-            (text_width, text_height), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_COMPLEX_SMALL, scale, thickness)
-            putText_ul(debug, text, fontScale=scale, thicknesss=thickness)
-            debug = cv2.bitwise_or(img, img, mask=debug)
-            if self.debug:
-                self.image_mux[3] = debug
+
+        # If at least one subscribe to debug image, enable producing of debug images
+        self.debug = self.debug_pub.im_pub.get_num_connections() > 0
+
+        # Crop image using ROI from object database
+        cropped = img[roi]
+        if self.debug:
+            self.image_mux[0] = cropped
+
+        # Get contour of LED panel from
+        match = self.get_stc_mask(cropped)
+        if match is not None: # Classify panel and look for pattern if match found
+            self.classify_panel(cropped, match)
+
+        # Publish debug image
         if self.debug:
             self.debug_pub.publish(self.image_mux())
 
